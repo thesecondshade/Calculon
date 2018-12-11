8631bb75837f6b223c00b24263b270b56242ec3e
Victoria Pierce <thesecondshade@piercefamily.name>
2018-12-11 14:56:05 -0600

    first commit

--- a5-code/calceval.ml
+++ a5-code/calceval.ml
@@ -143,7 +143,7 @@ let rec eval_expr varmap expr =
   | Lambda(l) ->                                                     (* lambda expressions *)
      begin
       let newclose = Closure {param_name = l.param_name; code_expr = l.code_expr; varmap = varmap;} in
-      let new_varmap = Varmap.add l.param_name newclose varmap
+      let new_varmap = Varmap.add l.param_name newclose varmap in
       eval_expr new_varmap l.code_expr
      end
 
